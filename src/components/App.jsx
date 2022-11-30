f4570409c18c89ad1a4e9cf0e1cd38b175b6db85
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-12-01 00:03:26 +0200

    error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,15 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { Card } from './Card/Card';
 import { ALL_COUNTRIES } from './config';
 import { Controls } from './Controls/Controls';
 import { Header } from './Header/Header';
+import { List } from './List/List';
 import { Main } from './Main/Main';
 
 export const App = () => {
   const [countries, setCountries] = useState([]);
-  console.log(countries);
+  // console.log(countries);
   useEffect(() => {
     axios.get(ALL_COUNTRIES).then(({ data }) => setCountries(data));
   }, []);
@@ -17,6 +19,32 @@ export const App = () => {
       <Header />
       <Main>
         <Controls />
+        <List>
+          {countries.length > 0 &&
+            countries.map(c => {
+              // console.log(c.name.common);
+              const countryInfo = {
+                img: c.flags.png,
+                name: c.name,
+                info: [
+                  {
+                    title: 'Population',
+                    description: c.population,
+                  },
+                  {
+                    title: 'Region',
+                    description: c.region,
+                  },
+                  {
+                    title: 'Capital',
+                    description: c.capital,
+                  },
+                ],
+              };
+              // return console.log(countryInfo);
+              return <Card key={c.name.common} {...countryInfo} />;
+            })}
+        </List>
       </Main>
     </>
   );
--- src/components/config.js
+++ src/components/config.js
@@ -1,6 +1,7 @@
 const BASE_URL = 'https://restcountries.com/v3.1/';
 
-export const ALL_COUNTRIES = BASE_URL + 'all';
+export const ALL_COUNTRIES =
+  BASE_URL + 'all?fields=name,capital,flags,populations,region';
 
 export const searchByCountry = name => BASE_URL + `name/` + name;
 export const filterhByCode = codes =>
